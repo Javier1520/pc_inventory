19e173c230cff6da8773617434ba54e8a7d7a08b
Javier Guerrero <jeguerrerocordoba@gmail.com>
2024-06-11 02:50:09 -0500

    the migrations run automatically

--- pc_inventory/settings.py
+++ pc_inventory/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os, time, django
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,6 +135,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATABASE_READY = False
+
+# At startup
+while not DATABASE_READY:
+  try:
+    django.db.connections['default'].ensure_connection()
+  except Exception:
+    time.sleep(1)
+  else:
+    DATABASE_READY = True
+
+# Run migrations now
+if not DATABASE_READY:
+  print('Applying migrations...')
+  os.system('python manage.py migrate')
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES':[
